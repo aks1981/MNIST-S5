7020c88691587cf7e38f90f09cecce0c0179ca08
aks1981 <me.ajitkumar@gmail.com>
2024-02-21 04:40:06 +0530

    Added model and utils files.

---
+++ model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+class Net(nn.Module):
+    #This defines the structure of the NN.
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        #self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(4096, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x): #28x28
+        x = F.relu(self.conv1(x), 2) #28 -> 26X32
+        x = F.relu(F.max_pool2d(self.conv2(x), 2)) #26 -> 12x64
+        x = F.relu(self.conv3(x), 2) #12 -> 10x128
+        x = F.relu(F.max_pool2d(self.conv4(x), 2)) #22 -> 4x256
+        x = x.view(-1, 4096)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
